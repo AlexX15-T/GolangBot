ea86cc799c3b806995e42c44a71483b898889799
Alex Tiugan <79551420+AlexX15-T@users.noreply.github.com>
2022-10-30 21:41:13 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -1,14 +1,78 @@
 package main
 
 import (
-	 f "fmt"
-	 "utility/jsonParser"
-	)
+	f "fmt"
+	"godiscordbot/handlers"
+	"godiscordbot/utility"
 
+	"github.com/bwmarrin/discordgo"
+)
 
-var cfg *Config
+var (
+	cfg   utility.Config
+	BotId string
+)
 
+func init() {
+	err := utility.GetConfig(&cfg)
+
+	if err != nil {
+		f.Println(err.Error())
+		return
+	}
+
+}
+
+func Start() {
+	goBot, err := discordgo.New("Bot " + cfg.Token)
+
+	if err != nil {
+		f.Println(err.Error())
+		return
+	}
+
+	u, err := goBot.User("@me")
+
+	if err != nil {
+		f.Println(err.Error())
+		return
+	}
+
+	BotId = u.ID
+	handlers.SetBotId(BotId)
+
+	goBot.AddHandler(MessageHandler)
+
+	err = goBot.Open()
+
+	if err != nil {
+		f.Println(err.Error())
+		return
+	}
+
+	f.Println("Everything is up..")
+}
 
 func main() {
-	f.Println("Hello fra", "my token is \n ")
+	Start()
+	<-make(chan struct{})
+	return
+}
+
+func MessageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == BotId {
+		return
+	}
+
+	var a utility.Config
+	err := utility.GetConfig(&a)
+
+	if err != nil {
+		f.Println(err.Error())
+		return
+	}
+
+	if m.Content == a.BotPrefix {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Noroc, frate!")
+	}
 }
